6e0b02fca428294f7f42416d2727e4bdcf76f080
Vidhya Varshini D <vidhyavarshinidcsbs2024@jerusalemengg.ac.in>
2025-12-22 15:38:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Future Developer" , "Computer Science Student" , "Tech learner" , "Aspiring Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
